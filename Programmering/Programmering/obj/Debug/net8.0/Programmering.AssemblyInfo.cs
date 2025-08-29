2a1d35fc726dff9ebadb0b97dc81db5a904582de
MrProgrammingMan <sander.josefsson@edu.varberg.se>
2025-08-29 14:29:47 +0200

    yes

--- Programmering/Programmering/obj/Debug/net8.0/Programmering.AssemblyInfo.cs
+++ Programmering/Programmering/obj/Debug/net8.0/Programmering.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Programmering")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ff058c14cfe65054dca377929bbd7d9e2d7ceb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Programmering")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Programmering")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
