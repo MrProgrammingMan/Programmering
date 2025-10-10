9f654904100599a71c5aecebac4968b5e449387f
MrProgrammingMan <sander.josefsson@edu.varberg.se>
2025-10-10 15:09:32 +0200

    Yatzy

---
+++ Programmering/Testa Skit Kod/Program.cs
@@ -0,0 +1,87 @@
+﻿using static System.Console;
+namespace Övningsprov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float störstaTal = float.MinValue;
+            float minstaTal = float.MaxValue;
+            int sum = 0;
+
+            while (true)
+            {
+                Write("Skirv in ett heltal (skriv 0 for att avsluta): ");
+                float heltal = float.Parse(ReadLine());
+
+                if (heltal != 0)
+                {
+                    Write("Vilket räknesätt vill du använda? (* / + eller -): ");
+                    string räknesätt = ReadLine();
+
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        if (räknesätt == "*")
+                        {
+                            WriteLine($"{i}x{heltal} = {heltal * i}");
+
+                            if (heltal * i < minstaTal)
+                            {
+                                minstaTal = heltal * i;
+                            }
+                            if (heltal * i > störstaTal)
+                            {
+                                störstaTal = heltal * i;
+                            }
+                        }
+                        else if (räknesätt == "/")
+                        {
+                            WriteLine($"{heltal}/{i} = {MathF.Round(heltal / i, 2)}");
+
+                            if (heltal / i < minstaTal)
+                            {
+                                minstaTal = MathF.Round(heltal / i, 2);
+                            }
+                            if (heltal / i > störstaTal)
+                            {
+                                störstaTal = heltal / i;
+                            }
+                        }
+                        else if (räknesätt == "+")
+                        {
+                            WriteLine($"{heltal} + {i} = {heltal + i}");
+
+                            if (heltal + i < minstaTal)
+                            {
+                                minstaTal = heltal + i;
+                            }
+                            if (heltal + i > störstaTal)
+                            {
+                                störstaTal = heltal + i;
+                            }
+                        }
+                        else if (räknesätt == "-")
+                        {
+                            WriteLine($"{heltal} - {i} = {heltal - i}");
+
+                            if (heltal - i < minstaTal)
+                            {
+                                minstaTal = heltal - i;
+                            }
+                            if (heltal - i > störstaTal)
+                            {
+                                störstaTal = heltal - i;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    WriteLine($"Största talet du fick var:{störstaTal}");
+                    WriteLine($"Minsta talet du fick var:{minstaTal}");
+                    break;
+                }
+            }
+        }
+    }
+}
--- Programmering/Yatzy/Yatzy/Program.cs
+++ Programmering/Yatzy/Yatzy/Program.cs
@@ -6,18 +6,135 @@ namespace Yatzy
     {
         static void Main(string[] args)
         {
-            Random rng = new Random();
-            List<int> tärningar = new();
-            for(int i = 0; i < 5; i++)
+            bool spela = true;
+            while (spela)
             {
-                tärningar.Add(rng.Next(0, 10));
+                int nuvarandeKast = 0;
+                Random rng = new Random();
+                List<int> tärningar = new();
+
+                slåTärningar(ref tärningar, rng);
+
+                int[] övreDelen = new int[6];
+                List<int> ettor = new();
+                List<int> tvår = new();
+                List<int> treor = new();
+                List<int> fyror = new();
+                List<int> femmor = new();
+                List<int> sexor = new();
+
+                bool rulla = true;
+                while (rulla)
+                {
+                    ettor.Clear();
+                    tvår.Clear();
+                    treor.Clear();
+                    fyror.Clear();
+                    femmor.Clear();
+                    sexor.Clear();
+
+                    for (int i = 0; i < tärningar.Count; i++)
+                    {
+                        switch (tärningar[i])
+                        {
+                            case 1:
+                                ettor.Add(tärningar[i]);
+                                break;
+                            case 2:
+                                tvår.Add(tärningar[i]);
+                                break;
+                            case 3:
+                                treor.Add(tärningar[i]);
+                                break;
+                            case 4:
+                                fyror.Add(tärningar[i]);
+                                break;
+                            case 5:
+                                femmor.Add(tärningar[i]);
+                                break;
+                            case 6:
+                                sexor.Add(tärningar[i]);
+                                break;
+                        }
+                    }
+                    string utskriftTop = String.Format("|{0,-6}|{1,-6}|{2,-6}|{3,-6}|{4,-6}|{5,-6}", "Ettor", "Tvår", "Treor", "Fyror", "Femmor", "Sexor");
+                    string poäng = String.Format("|{0,-6}|{1,-6}|{2,-6}|{3,-6}|{4,-6}|{5,-6}", 1 * ettor.Count + "p", 2 * tvår.Count + "p", 3 * treor.Count + "p", 4 * fyror.Count + "p", 5 * femmor.Count + "p", 6 * sexor.Count + "p");
+                    string utskrift = String.Format("|{0,-6}|{1,-6}|{2,-6}|{3,-6}|{4,-6}|{5,-6}",
+                    string.Join(",", ettor),
+                    string.Join(",", tvår),
+                    string.Join(",", treor),
+                    string.Join(",", fyror),
+                    string.Join(",", femmor),
+                    string.Join(",", sexor));
+                    WriteLine(utskriftTop);
+                    WriteLine("-----------------------------------------");
+                    foreach (var item in utskrift)
+                    {
+                        if (item != ' ' && item != '|' && item != ',')
+                        {
+                            Thread.Sleep(600);
+                        }
+                        Write(item);
+                    }
+                    WriteLine("");
+                    WriteLine("-----------------------------------------");
+                    WriteLine(poäng);
+
+                    rollaOm(ref tärningar, ref spela, rng, ref nuvarandeKast);
+                    spela = true;
+                }
             }
-            List<int> ettor = new();
-            List<int> tvår = new();
-            List<int> treor = new();
-            List<int> fyror = new();
-            List<int> femmor = new();
-            List<int> sexor = new();
+        }
+        static void slåTärningar(ref List<int> tärningar, Random rng)
+        {
+            tärningar.Clear();
+            for (int i = 0; i <= 5; i++)
+            {
+                tärningar.Add(rng.Next(1, 7));
+            }
+        }
+        static void rollaOm(ref List<int> tärningar, ref bool spela, Random rng, ref int nuvarandeKast)
+        {
+            int maxKast = 2;
+
+            while (true)
+            {
+                tärningar.Clear();
+                WriteLine("");
+                WriteLine("Vill du kasta om dina tärningar? Du får kasta om max 2 gånger. [Y/N]");
+                char input = char.ToLower(ReadKey().KeyChar);
+                WriteLine("");
+
+                if (input == 'y')
+                {
+                    if (nuvarandeKast < maxKast)
+                    {
+                        nuvarandeKast++;
+                        Clear();
+                        WriteLine($"Kastar om...");
+                        slåTärningar(ref tärningar, rng);
+                        break;
+                    }
+                    else
+                    {
+                        WriteLine("Du har redan rollat 2 gånger, du behöver nu välja de nummerna du vill använda: 1, 2, 3, 4, 5, 6");
+                        nuvarandeKast = 0;
+                        spela = false;
+                        break;
+                    }
+                }
+
+                else if (input == 'n')
+                {
+                    WriteLine("Du valde att inte kasta om, välj vilka nummer du villa ha.");
+                    nuvarandeKast = 0;
+                    break;
+                }
+                else
+                {
+                    WriteLine("Tryck på [Y/N]");
+                }
+            }
         }
     }
 }
