96d253f1c0859007ba5c6d0378887927538bf75b
MrProgrammingMan <sander.josefsson@edu.varberg.se>
2025-10-24 12:20:56 +0200

    number guessing

--- Programmering/Number guessing/Number guessing/Program.cs
+++ Programmering/Number guessing/Number guessing/Program.cs
@@ -8,29 +8,59 @@ namespace Number_guessing
             int currentNumber = 0;
             int min = 1;
             int max = 101;
+            int currentGuesses = 0;
             Random rng = new Random();
-
 
             currentNumber = rng.Next(min, max);
 
-            while(true)
+            while (true)
             {
                 WriteLine("Enter a number between 1-100.");
 
-                int input = int.Parse(ReadLine());
+                string? input = ReadLine();
 
-                    if (input < currentNumber)
+                if (int.TryParse(input, out int result)) 
+                {
+                    if (result < currentNumber)
                     {
                         WriteLine("Bigger");
+                        currentGuesses++;
                     }
-                    else if (input > currentNumber)
+                    else if (result > currentNumber)
                     {
                         WriteLine("Smaller");
+                        currentGuesses++;
                     }
+                }
 
-                if (input == currentNumber)
+                if (result == currentNumber)
                 {
-                    WriteLine("You got it!");
+                    WriteLine($"You got it! the number was {currentNumber}!");
+                    WriteLine();
+                    WriteLine($"It took you {currentGuesses} guesses!");
+
+                    currentGuesses = 0;
+
+                    while (true)
+                    {
+                        WriteLine("Do you wish to play again? [Y/N]");
+                        char key = char.ToLower(ReadKey(true).KeyChar);
+
+                        if (key == 'y')
+                        {
+                            Clear();
+                            currentNumber = rng.Next(min, max);
+                            break;
+                        }
+                        else if (key == 'n')
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            WriteLine("Type [Y/N]");
+                        }
+                    }
                 }
             }
         }
